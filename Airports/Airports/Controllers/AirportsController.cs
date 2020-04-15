b39bb141f5d0c8ed9f972cce54e309d2daebcf5d
adriantompa <adrianliviu18@gmail.com>
2020-04-15 11:22:54 +0300

    Add files via upload

---
+++ Airports/Airports/Controllers/AirportsController.cs
@@ -0,0 +1,47 @@
+﻿using AirportsApi.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using AirportsApi.DataContracts;
+using AirportsApi.Configuration;
+using AirportsApi.Services.Interfaces;
+
+namespace Airports.Controllers
+{
+    [Route("api/[controller]")]
+    public class AirportsController : Controller
+    {
+        private readonly IAirportsService _airportsService;
+
+        public AirportsController(IAirportsService airportsService)
+        {
+            _airportsService = airportsService;
+        }
+
+        [HttpGet("{from}/{to}")]
+        public IActionResult MeasureDistanceInMiles(string from, string to)
+        {
+            if (!Validation.IsAirportCodeValid(from))
+                return this.BadRequest(AppConfig.NOT_VALID_AIRPORT_FROM);
+
+            if (!Validation.IsAirportCodeValid(to))
+                return this.BadRequest(AppConfig.NOT_VALID_AIRPORT_TO);
+
+            var airportFrom = _airportsService.GetAirportLocation(from.ToUpper());
+            if (airportFrom == null)
+                return this.BadRequest(AppConfig.NOT_VALID_AIRPORT_FROM);
+
+            var airportTo = _airportsService.GetAirportLocation(to.ToUpper());
+            if (airportTo ==null)
+                return this.BadRequest(AppConfig.NOT_VALID_AIRPORT_TO);
+
+            double miles = _airportsService.GetDistanceInMiles(airportFrom, airportTo);
+
+            var response = new AirportResponse()
+            {
+                UnitValue = miles,
+                UnitName = AppConfig.MILES_UNIT
+            };
+
+            return this.Ok(response);
+        }
+    }
+}
---
+++ Airports/Airports/Helpers/Validation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace AirportsApi.Helpers
+{
+    public static class Validation
+    {
+        public static bool IsAirportCodeValid(string code)
+        {
+            return !string.IsNullOrEmpty(code) && code.Length == 3 && code.All(Char.IsLetter);
+        }
+    }
+}
